142f1a38141fae94ad8dc6ac6177d5c85ac4e7cd
Vijay M Giri <vijaymgiri@gmail.com>
2017-04-18 20:31:38 +0530

    level 2 transition

--- client.c
+++ client.c
@@ -17,11 +17,12 @@
 #define P2 2
 #define P3 3
 #define P4 4
-#define BULLET 5
-#define BRICK 6
+#define BULLET 11
+#define BRICK 12
 #define BLANK 0
 #define DIMENSION 80
-
+#define DIMENSION2 50
+#define DIMENSION1 100
 #define KNRM  "\x1B[0m"
 #define KRED  "\x1B[31m"
 #define KGRN  "\x1B[32m"
@@ -38,6 +39,7 @@ typedef struct {
 	int x,y;
 	int health;
 	int points;
+	int teamNo;
 }CLIENT;
 
 typedef struct {
@@ -52,15 +54,16 @@ typedef struct {
 
 typedef struct {
 	CLIENT clients[MAX_PLAYERS];
-	CELL matrix[DIMENSION][DIMENSION];
+	CELL matrix[DIMENSION2][DIMENSION1];
 	int num_players;
 	char msg[1024];
+	int sqNo;
 }SEND;
 
 SEND receive;
 
 char buffer[1024];
-int myIndex,isAlive;
+int myIndex,isAlive,curSeq;
 
 void getInput(char* c){
 	struct pollfd mypoll = { STDIN_FILENO, POLLIN|POLLPRI };
@@ -72,7 +75,6 @@ void getInput(char* c){
 		return ;
 	}
 	system("/bin/stty cooked");
-	*c = '$';
 }
 
 char get()
@@ -91,23 +93,74 @@ char get()
 	return ch;
 }
 
+void print_matrix(int i, int j){
+
+	if(receive.matrix[i][j].type==P1){
+	if(receive.matrix[i][j].direction==UP)
+	printf("%s%c",KRED,'^');
+	else if(receive.matrix[i][j].direction==DOWN)
+	printf("%s%c",KRED,'v');
+	else if(receive.matrix[i][j].direction==LEFT)
+	printf("%s%c",KRED,'<');
+	else if(receive.matrix[i][j].direction==RIGHT)
+	printf("%s%c",KRED,'>');
+	}
+	else if(receive.matrix[i][j].type==P2){
+	if(receive.matrix[i][j].direction==UP)
+	printf("%s%c",KGRN,'^');
+	else if(receive.matrix[i][j].direction==DOWN)
+	printf("%s%c",KGRN,'v');
+	else if(receive.matrix[i][j].direction==LEFT)
+	printf("%s%c",KGRN,'<');
+	else if(receive.matrix[i][j].direction==RIGHT)
+	printf("%s%c",KGRN,'>');
+	}
+	else if(receive.matrix[i][j].type==P3){
+	if(receive.matrix[i][j].direction==UP)
+	printf("%s%c",KMAG,'^');
+	else if(receive.matrix[i][j].direction==DOWN)
+	printf("%s%c",KMAG,'v');
+	else if(receive.matrix[i][j].direction==LEFT)
+	printf("%s%c",KMAG,'<');
+	else if(receive.matrix[i][j].direction==RIGHT)
+	printf("%s%c",KMAG,'>');
+	}
+	else if(receive.matrix[i][j].type==P4){
+	if(receive.matrix[i][j].direction==UP)
+	printf("%s%c",KBLU,'^');
+	else if(receive.matrix[i][j].direction==DOWN)
+	printf("%s%c",KBLU,'v');
+	else if(receive.matrix[i][j].direction==LEFT)
+	printf("%s%c",KBLU,'<');
+	else if(receive.matrix[i][j].direction==RIGHT)
+	printf("%s%c",KBLU,'>');
+	}
+	else if(receive.matrix[i][j].type==BRICK)
+	printf("%s%c",KYEL,'|');
+	else if(receive.matrix[i][j].type==BULLET)
+	printf("%s%c",KCYN,'-');
+	else
+	printf("%s%c",KWHT,'*');
+
+}
+
 void display(){
+	isAlive = 0;
 	printf("\033[2J\033[1;1H");
 	int i,j;
 	printf("Nick\t\tPoints\t\t\t Your health : %d\n",receive.clients[myIndex].health);
+	printf("Sequence no. %d\n",curSeq);
 	for(i=0;i<=receive.num_players;i++){
 		printf("%s\t\t%d\n",receive.clients[i].name,receive.clients[i].points);	
 	}
 	printf("\n\n");
-	for(i=0;i<30;i++){
-		for(j=0;j<DIMENSION;j++){
-			if(receive.matrix[i][j].type==P1){
-			printf("%s%d",KRED,receive.matrix[i][j].type);
-			}	
-			else if(receive.matrix[i][j].type==BRICK)
-			printf("%s%d",KYEL,receive.matrix[i][j].type);
-			else
-			printf("%s%d",KWHT,receive.matrix[i][j].type);
+	for(i=0;i<DIMENSION2;i++){
+		for(j=0;j<DIMENSION1;j++){
+			if( receive.matrix[i][j].type == myIndex+1 ){
+				isAlive = 1;
+			}
+				
+			print_matrix(i,j);
 		}
 		printf("\n");
 	}
@@ -150,7 +203,6 @@ int main()
 	while(1){ // Ready and not ready block
 
 		n = recvfrom(socketfd,buffer,1024,MSG_DONTWAIT,(struct sockaddr*)&serverAddr,&addr_size);
-		//printf("%s %d \n",buffer,n);
 		int flag = 0;
 		if(n > 0){
 			flag = 1;
@@ -178,7 +230,7 @@ int main()
 		}
 		if(flag) printf("\t\t\t\t\t\n Ready for the game? (y/n)");
 		fflush(stdout);
-		char c; 
+		char c = '$'; 
 		getInput(&c);
 		if(c == '*') break;
 		if(c != 'y' && c != 'n') continue;
@@ -189,19 +241,26 @@ int main()
 		sendto(socketfd,buffer,1024,0,(struct sockaddr*)&serverAddr,addr_size);
 	}
 	
+	printf("here\n");
 	n = recvfrom(socketfd,&receive,sizeof(SEND),0,(struct sockaddr*)&serverAddr,&addr_size);
+	printf("received\n");
 	if(n > 0){ // sending acknowledgment
 		myIndex = receive.msg[0] - '0';
+		printf("Received matrix %d %d %d\n",myIndex,receive.num_players,n);
+		curSeq = receive.sqNo;
 		buffer[0] = '$'; buffer[1] = '\0';
 		sendto(socketfd,buffer,1024,0,(struct sockaddr*)&serverAddr,addr_size);
+		printf("ACK sent %d\n",curSeq);
 	}
 	display();
 	buffer[1] = '*';
 	buffer[2] = '\0';
 	isAlive = 1;
+	int gameEnd = 0;
 	while(1){ // Game starts
 		buffer[0] = '$';
-		buffer[0] = get();
+		//buffer[0] = get();
+		getInput(&buffer[0]);
 		if(buffer[0] == '*')
 			break;
 		if( isAlive && (buffer[0] == ' ' || buffer[0] == 'd' || buffer[0] == 's' || buffer[0] == 'a' || buffer[0] == 'w')){
@@ -209,11 +268,69 @@ int main()
 		}
 		while(1){
 			n = recvfrom(socketfd,&receive,sizeof(SEND),MSG_DONTWAIT,(struct sockaddr*)&serverAddr,&addr_size);
-			if(n > 0){
+			if(n > 0 && curSeq > receive.sqNo){
+				curSeq = receive.sqNo;
+				if(receive.msg[0] == '*' && receive.msg[1] == '*' && receive.msg[2] == '*'){ // level 1 ends
+					gameEnd = 1;
+					buffer[0] = '$'; buffer[1] = '\0';
+					printf("Level 1 ends !!");
+					sendto(socketfd,buffer,1024,0,(struct sockaddr*)&serverAddr,addr_size);
+					printf("Level 1 ends !!");
+					break;
+				}
 				display();
 			}
 			else break;
 		}
+		if(gameEnd) break;
+	}
+	printf("Level 1 ends!!\n");
+	n = recvfrom(socketfd,buffer,1024,0,(struct sockaddr*)&serverAddr,&addr_size);
+	char team = '$';
+	while(1){ // level 2 starting 
+		int flag = 0;
+		if(n > 0){
+			if(buffer[0] == '$'){
+				printf("\t\t\t\tLevel 2 is about to start !! \n");
+				break;
+			}
+			flag = 1;
+			printf("\t\t\t\tPlayers currently connected \n\n");
+			i = 0;	
+			printf("%s\n",buffer);
+			while(buffer[i]){
+				if(buffer[i] == '*')									
+					printf("\n\t\t\t\t");
+				else if(buffer[i] == '$'){
+					printf("\t\t\t\t");
+					if(buffer[i+1] == 1) printf("Ready\t\t\t\t");
+					else printf("Not Ready\t\t\t\t");
+					printf("%c Team \t\t\t\t",buffer[i+2]);
+					i += 2;
+				}
+				else printf("%c",buffer[i]);
+				i++;
+			}
+			printf("\n");
+		}
+		if(flag){
+			printf("\t\t\t\tWhich team do you want to join ?? (range 1-9). Are you ready for the game ?? (y or n) : format ex -> y/2 \n");
+		}
+		getInput(&team);
+		char c; 
+		getInput(&c);
+		if(team == '*') break;
+		printf("\n");
+		if(c != 'y' && c != 'n'){
+			n = recvfrom(socketfd,buffer,1024,MSG_DONTWAIT,(struct sockaddr*)&serverAddr,&addr_size);
+			continue;
+		}
+		buffer[0] = '*';
+		if(c == 'y') buffer[1] = '1';
+		else buffer[1] = '2';
+		buffer[2] = team;
+		sendto(socketfd,buffer,1024,0,(struct sockaddr*)&serverAddr,addr_size);
+		n = recvfrom(socketfd,buffer,1024,MSG_DONTWAIT,(struct sockaddr*)&serverAddr,&addr_size);
 	}
 	return 0;
 }
